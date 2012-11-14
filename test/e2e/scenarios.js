a3bb0c614d57afe66b99078082c5648eeb332862
Kim Pettersen <kim.a.pettersen@gmail.com>
2012-11-14 11:39:30 -0500

    Changed tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,40 +1,41 @@
 describe('PhoneCat App', function() {
- 
   describe('Phone list view', function() {
- 
-    beforeEach(function() {
-      browser().navigateTo('/phones');
-    });
- 
- 
-    it('should filter the phone list as user types into the search box', function() {
-      expect(repeater('.phones li').count()).toBe(3);
- 
-      input('query').enter('nexus');
-      expect(repeater('.phones li').count()).toBe(1);
- 
-      input('query').enter('motorola');
-      expect(repeater('.phones li').count()).toBe(2);
-    });
 
-    it('should display the current filter value within an element with id "status"', function(){
-        expect(element('#status').text()).toMatch(/Current filter: \s*$/);
-        // pause()
+      beforeEach(function() {
+        browser().navigateTo('../../app/index.html#/phones');
+      });
+
+
+      it('should filter the phone list as user types into the search box', function() {
+        expect(repeater('.phones li').count()).toBe(20);
+
         input('query').enter('nexus');
+        expect(repeater('.phones li').count()).toBe(1);
 
-        expect(element('#status').text()).toMatch(/Current filter: nexus\s*$/);
-    });
+        input('query').enter('motorola');
+        expect(repeater('.phones li').count()).toBe(8);
+      });
 
-    it('should be possible to control phone order via drop down select box', function(){
-        input('query').enter('tablet');
 
-        expect(repeater('.phones li', 'Phone List').column('phone.name')).toEqual(["Motorola XOOM\u2122 with Wi-Fi",
-                   "MOTOROLA XOOM\u2122"]);
+      it('should be possible to control phone order via the drop down select box', function() {
+        input('query').enter('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+        expect(repeater('.phones li', 'Phone List').column('phone.name')).
+            toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                     "MOTOROLA XOOM\u2122"]);
 
         select('orderProp').option('Alphabetical');
- 
-        expect(repeater('.phones li', 'Phone List').column('phone.name')).toEqual(["MOTOROLA XOOM\u2122",
-                   "Motorola XOOM\u2122 with Wi-Fi"]);
+
+        expect(repeater('.phones li', 'Phone List').column('phone.name')).
+            toEqual(["MOTOROLA XOOM\u2122",
+                     "Motorola XOOM\u2122 with Wi-Fi"]);
+      });
+
+
+      it('should render phone specific links', function() {
+        input('query').enter('nexus');
+        element('.phones li a').click();
+        expect(browser().location().url()).toBe('/phones/nexus-s');
+      });
     });
-  });
 });
